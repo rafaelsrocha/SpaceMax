713455d1aa7ac5f176a6e12926dbb3a1c16bbcf3
Rafael Rocha <rocha.development@gmail.com>
2023-04-20 19:56:26 +0200

    Create NextLaunchCountdown

---
+++ SpaceMax/Features/Launches/NextLaunch/Views/NextLaunchCountdown.swift
@@ -0,0 +1,31 @@
+//
+//  NextLaunchCountdown.swift
+//  SpaceMax
+//
+//  Created by Rafael Rocha on 20.04.23.
+//
+
+import SwiftUI
+
+struct NextLaunchCountdown: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            NextLaunchCountdownTicker()
+            Spacer()
+            NextLaunchCountdownTicker()
+            Spacer()
+            NextLaunchCountdownTicker()
+            Spacer()
+            NextLaunchCountdownTicker()
+            Spacer()
+        }
+    }
+}
+
+struct NextLaunchCountdown_Previews: PreviewProvider {
+    static var previews: some View {
+        NextLaunchCountdown()
+            .background(AppTheme.screenBackground)
+    }
+}
---
+++ SpaceMax/Features/Launches/NextLaunch/Views/NextLaunchCountdownTicker.swift
@@ -0,0 +1,24 @@
+//
+//  NextLaunchCountdownTicker.swift
+//  SpaceMax
+//
+//  Created by Rafael Rocha on 20.04.23.
+//
+
+import SwiftUI
+
+struct NextLaunchCountdownTicker: View {
+    var body: some View {
+        VStack {
+            Text("13").smText(size: 60)
+            Text("days").smText(.title3)
+        }
+    }
+}
+
+struct NextLaunchCountdownTicker_Previews: PreviewProvider {
+    static var previews: some View {
+        NextLaunchCountdownTicker()
+            .background(AppTheme.screenBackground)
+    }
+}
