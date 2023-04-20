780fe04c7a504bf4a15c48e090cac842841e4f42
Rafael Rocha <rocha.development@gmail.com>
2023-04-20 16:20:53 +0200

    Create AppTheme.swift

---
+++ SpaceMax/Core/Design/AppTheme.swift
@@ -0,0 +1,22 @@
+//
+//  AppTheme.swift
+//  SpaceMax
+//
+//  Created by Rafael Rocha on 19.04.23.
+//
+
+import SwiftUI
+
+struct AppTheme {
+    private init() {}
+    /// Dark gray color
+    private static let darkGray30 = Color(red: 33/255, green: 35/255, blue: 37/255)
+    /// Screen / Page background color
+    static var screenBackground: Color { .black }
+    /// Section / Card background color
+    static var sectionBackground: Color { darkGray30 }
+    /// App font's primary color
+    static var primaryFontColor: Color { .white }
+    /// App font's secondary color
+    static var secondaryFontColor: Color { .gray }
+}
