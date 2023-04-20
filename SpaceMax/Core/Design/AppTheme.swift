1daa5b596f81c02bd4fe09c381ac9e8d9404e0ab
Rafael Rocha <rocha.development@gmail.com>
2023-04-20 19:57:42 +0200

    Change secondaryFontColor

--- SpaceMax/Core/Design/AppTheme.swift
+++ SpaceMax/Core/Design/AppTheme.swift
@@ -11,6 +11,8 @@ struct AppTheme {
     private init() {}
     /// Dark gray color
     private static let darkGray30 = Color(red: 33/255, green: 35/255, blue: 37/255)
+    /// Light gray color
+    private static let lightGray80 = Color(red: 200/255, green: 200/255, blue: 200/255)
     /// Screen / Page background color
     static var screenBackground: Color { .black }
     /// Section / Card background color
@@ -18,5 +20,5 @@ struct AppTheme {
     /// App font's primary color
     static var primaryFontColor: Color { .white }
     /// App font's secondary color
-    static var secondaryFontColor: Color { .gray }
+    static var secondaryFontColor: Color { lightGray80 }
 }
